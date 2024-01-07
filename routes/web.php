730189c9d773fc1435ea60dfeca1d3a32cc7070b
ilfizamutiarahmi <ilfiza.mutiarahmi@gmail.com>
2024-01-07 10:11:08 +0700

    add dosen

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,16 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
+//dosen
+Route::get('/dosen','App\Http\Controllers\DosenController@index')->name('dosen.index');
+Route::get('/dosen/create','App\Http\Controllers\DosenController@create')->name('dosen.create');
+Route::post('/dosen', 'App\Http\Controllers\DosenController@store')->name('dosen.store');
+
+//mahasiswa
+Route::get('/mahasiswa','App\Http\Controllers\MahasiswaController@index')->name('mahasiswa.index');
+Route::get('/mahasiswa/create','App\Http\Controllers\MahasiswaController@create')->name('mahasiswa.create');
+Route::post('/mahasiswa', 'App\Http\Controllers\MahasiswaController@store')->name('mahasiswa.store');
+Route::get('/mahasiswa/edit/{id}','App\Http\Controllers\MahasiswaController@edit')->name('mahasiswa.edit');
+Route::post('/mahasiswa/$id', 'App\Http\Controllers\MahasiswaController@update')->name('mahasiswa.update');
+
 // Route::get('dashboard',HomeController::class,'dashboard');
